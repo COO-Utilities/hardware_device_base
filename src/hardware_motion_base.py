8c406e907333b3b9d1d7521074ce05494864f46e
Don Neill <neilljd@gmail.com>
2025-11-17 11:49:55 -0800

    update to hardware_motion_base.py

--- src/hardware_motion_base.py
+++ src/hardware_motion_base.py
@@ -5,7 +5,6 @@ Defines an abstract base class for any device.
 method list:
     Queries:
         is_active
-        is_connected
         is_closed_loop
         get_error
         get_pos
@@ -20,10 +19,9 @@ method list:
         set_pos
         load_presets
 """
-import threading
-import logging
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from typing import Union
+
 from hardware_device_base import HardwareDeviceBase
 
 class HardwareMotionBase(HardwareDeviceBase):
@@ -33,149 +31,26 @@ class HardwareMotionBase(HardwareDeviceBase):
     This class defines the interface for interacting with the hardware motion device.
 
     Subclasses must implement the following public methods:
-        * `get_status()`: Get an atomic telemetry value.
+        * `get_status()`: Get a status string of the hardware motion device.
 
     Subclasses must also implement the following private methods:
-        * `_send_command()`: Send a command.
-        * `_read_reply()`: Receive a reply.
+
 
     Subclasses may optionally override or use the following concrete methods:
-        * `validate_connection_params()`: Validate the connection parameters.
-        * `set_verbose()`: Set the verbose level to include DEBUG logging (True) or not (False).
-        * `is_connected()`: Return True if the connection is active.
-        * `_set_connected()`: Set the connected status.
 
 
     See example_hardware_stage.py for example usage.
     """
-
-    def __init__(self, log: bool =True, logfile: str = None):
-        """Instantiate the device class
-
-        :param bool log: If True, log to file, otherwise log to stdout.
-        :param str logfile: filename to log to.
-
-        """
-        super().__init__()
-
-        # thread lock
-        self.lock = threading.Lock()
-
-        # connection status
-        self.connected = False
-
-        # set up logging
-
-        self.verbose = False
-        if logfile is None:
-            logfile = __name__.rsplit(".", 1)[-1]
-        self.logger = logging.getLogger(logfile)
-        self.logger.setLevel(logging.INFO)
-        # log to console by default
-        console_formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s')
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(console_formatter)
-        if not self.logger.hasHandlers():
-            self.logger.addHandler(console_handler)
-            # log to file if requested
-            if log:
-                formatter = logging.Formatter(
-                    "%(asctime)s - %(levelname)s - %(funcName)s() - %(message)s"
-                )
-                file_handler = logging.FileHandler(
-                    logfile if ".log" in logfile else logfile + ".log")
-                file_handler.setFormatter(formatter)
-                self.logger.addHandler(file_handler)
-
-    # public abstract methods
     @abstractmethod
-    def connect(self, *args, con_type: str ="tcp") -> None:
-        """Establishes a connection to the device.
-
-        :param args: Positional arguments to pass to the constructor.
-        :param str con_type: Type of connection to establish: serial or tcp.
-
-        The arguments should only provide what is needed to establish a connection.
-        Socket Example:
-            self.connect("127.0.01", 9999)
-        Serial Example:
-            self.connect("/dev/tty1", 9600, con_type="serial")
-        """
-        self.connected = True
-
-    @abstractmethod
-    def disconnect(self) -> None:
-        """Disconnects from the device."""
-        self.connected = False
-
-    @abstractmethod
-    def get_atomic_value(self, item: str ="") -> Union[float, int, str, None]:
-        """Returns the value from the specified item.
-        :param str item: item to get the value from.
-        :return: value from the specified item.
-        """
-        return NotImplemented
-
-    # private abstract methods
-    @abstractmethod
-    def _send_command(self, command: str, *args) -> bool:
-        """Send a command to the device.
-
-        :param str command: Command to send.
-        :param args: Positional arguments to pass to the constructor.
-        :return: True if command was sent, False otherwise.
-
-        This command should lock the thread when sending the command
-        Example:
-            with self.lock:
-                self.sock.sendall(command.encode())
+    def get_status(self) -> Union[str, None]:
+        """Get the status of the hardware motion device.
+        :return: The status of the hardware motion device.
         """
         return NotImplemented
 
     @abstractmethod
-    def _read_reply(self) -> Union[str, None]:
-        """Receive a reply from the device.
-        :return: The reply or None if no reply was received."""
+    def is_active(self) -> bool:
+        """Check if the hardware motion device is active."""
         return NotImplemented
-
-    # public concrete methods
-    def set_verbose(self, verbose: bool =True) -> None:
-        """Sets verbose mode.
-        :param bool verbose: Verbose mode: True (default) DEBUG level or False INFO level.
-        """
-        self.verbose = verbose
-        self.logger.setLevel(logging.DEBUG if verbose else logging.INFO)
-        self.logger.debug("Verbose mode: %s", verbose)
-
-    def is_connected(self) -> bool:
-        """Optional concrete method that subclasses may override.
-
-        Returns:
-            bool: Connection status.
-        """
-        return self.connected
-
-    def validate_connection_params(self, *args) -> bool:
-        """Validates connection parameters.
-        :param args: Positional arguments for connect method.
-        """
-        if len(args[0]) < 2:
-            self.logger.error("connect requires two connection parameters: %s", args)
-            return False
-        if not isinstance(args[0][0], str):
-            self.logger.error("First argument must be a string.")
-            return False
-        if not isinstance(args[0][1], int):
-            self.logger.error("Second argument must be a integer.")
-            return False
-        return True
-
-    # private concrete methods
-    def _set_connected(self, connected: bool) -> None:
-        """Optional concrete method that subclasses may override.
-
-        :param bool connected: Whether the device connection has already been established.
-        :return: None
-        """
-        self.connected = connected
+    @abstractmethod
+    def
